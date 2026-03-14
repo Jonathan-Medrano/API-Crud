8f21554d2b08ac980cb92e966152343537edd348
Jonathan Medrano <jona.medrano@trizap.net>
2026-03-14 19:22:35 -0300

    Obtener empleado por ID

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -4,37 +4,46 @@ import { pool } from "../db.js";
 export const getEmployees = async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM empleados")
-    if (result.lenght === 0) {
+    if (result.length === 0) {
       res.send("No hay empleados")
     } else {
       res.send(result[0]);
     }
   } catch (error) {
-    res.status(404).send({ response: "error al obtener empleados" });
+    res.status(500).send({ response: "error al obtener empleados" });
   }
 }
 
 //Obtener empleado por ID
-export const getEmployeeById = (req, res) => {
-  res.send("get employee");
+export const getEmployeeById = async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT * FROM empleados WHERE id = ?", [req.params.id])
+    if (result.length === 0) {
+      res.send("Empleado no encontrado")
+    } else {
+      res.send(result[0]);
+    }
+  } catch (error) {
+    res.status(500).send({ response: "error al obtener empleado" });
+  }
 }
 
 //Eliminar empleado
-export const deleteEmployeeById = (req, res) => {
+export const deleteEmployeeById = async (req, res) => {
   res.send("delete employees");
 }
 
 //Editar empleado
-export const editEmployeeById = (req, res) => {
+export const editEmployeeById = async (req, res) => {
   res.send("edit employee");
 }
 
 //Crear empleado
-export const createEmployee = (req, res) => {
+export const createEmployee = async (req, res) => {
   res.send("create employee");
 }
 
 //Editar un atributo de un empleado
-export const patchEmployee = (req, res) => {
+export const patchEmployee = async (req, res) => {
   res.send("edit employee attribute");
 }
